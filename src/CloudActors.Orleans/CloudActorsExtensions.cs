f4131d67ff773d1d7558fd19c59941691d1d9fe6
Daniel Cazzulino <daniel@cazzulino.com>
2023-08-08 13:41:14 -0300

    Simplify silo/services configuration and streamstone config

--- src/CloudActors.Orleans/CloudActorsExtensions.cs
+++ src/CloudActors.Orleans/CloudActorsExtensions.cs
@@ -1,96 +1,53 @@
 ﻿using System;
-using System.Buffers;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using Devlooped.CloudActors;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Orleans;
-using Orleans.Core;
+using Orleans.Hosting;
 using Orleans.Runtime;
 
-[EditorBrowsable(EditorBrowsableState.Never)]
-public static class CloudActorsExtensions
+namespace Devlooped.CloudActors
 {
-    /// <summary>
-    /// Adds the <see cref="IActorBus"/> service and custom actor activation logic.
-    /// </summary>
-    public static IServiceCollection AddCloudActors(this IServiceCollection services)
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static partial class CloudActorsExtensions
     {
-        services.TryAddSingleton<IActorBus>(sp => new OrleansActorBus(sp.GetRequiredService<IGrainFactory>()));
-
-        // Attempt to replace the OOB persistence so we don't require a parameterless constructor and always 
-        // have actors initialized with a specific id from the grain.
-        if (services.FirstOrDefault(d => d.ServiceType == typeof(IPersistentStateFactory)) is { } descriptor)
+        /// <summary>
+        /// Adds the <see cref="IActorBus"/> service and actor activation logic. 
+        /// </summary>
+        /// <remarks>
+        /// It's not necessary to invoke this method if you already invoked <c>AddCloudActors</c> 
+        /// on the <see cref="ISiloBuilder"/> when configuring Orleans.
+        /// </remarks>
+        public static IServiceCollection AddCloudActors(this IServiceCollection services)
         {
-            services.Remove(descriptor);
-            services.Replace(ServiceDescriptor.Describe(
-              typeof(IPersistentStateFactory),
-              s => new ActorActivatorFactory((IPersistentStateFactory)CreateInstance(s, descriptor)),
-              descriptor.Lifetime
-            ));
-        }
-        return services;
-    }
-
-    static object CreateInstance(IServiceProvider services, ServiceDescriptor descriptor)
-    {
-        if (descriptor.ImplementationInstance is not null)
-            return descriptor.ImplementationInstance;
-
-        if (descriptor.ImplementationFactory is not null)
-            return descriptor.ImplementationFactory(services);
-
-        return ActivatorUtilities.GetServiceOrCreateInstance(services, descriptor.ImplementationType!);
-    }
-
-    class ActorActivatorFactory(IPersistentStateFactory factory) : IPersistentStateFactory
-    {
-        public IPersistentState<TState> Create<TState>(IGrainContext context, IPersistentStateConfiguration config)
-        {
-            var state = factory.Create<TState>(context, config);
-            // We're super conservative here, only replacing if all conditions are met.
-            // We check for a state with no parameterless constructor up-front, then go from there.
-            if (typeof(TState).GetConstructor(Type.EmptyTypes) is null &&
-                // We only know how the state bridge works
-                state is StateStorageBridge<TState> bridge &&
-                // We only support a ctor that receives the grain id as a string
-                context.GrainId.Key.ToString() is object id &&
-                typeof(TState).GetConstructor(new[] { typeof(string) }) is not null &&
-                // Even so, we might fail activation
-                Activator.CreateInstance(typeof(TState), id) is TState actor)
+            services.TryAddSingleton<IActorBus>(sp => new OrleansActorBus(sp.GetRequiredService<IGrainFactory>()));
+            
+            // Attempt to replace the OOB persistence so we don't require a parameterless constructor and always 
+            // have actors initialized with a specific id from the grain.
+            if (services.FirstOrDefault(d => d.ServiceType == typeof(IActorStateFactory)) is null &&
+                services.FirstOrDefault(d => d.ServiceType == typeof(IPersistentStateFactory)) is { } descriptor)
             {
-                // In this case, we can force custom creation of the state object.
-                // Internally, this causes the StateStorageBridge<T>._grainState to not be 
-                // forcedly constructed via Activator.CreateInstance with a parameterless constructor, 
-                // and instead our "rehydrated" type is used instead.
-                // NOTE: this causes the OnStart on the PersistentState<TState> class to skip invoking 
-                // ReadStateAsync, as it assumes rehydration makes that unnecessary. 
-                // However, the JournaledGrain base class overrides the OnActivateAsync method and 
-                // forces a sync, so we're good since our generated grain does that too.
-                bridge.OnRehydrate(new ActivationContext(new GrainState<TState>(actor)));
+                services.Remove(descriptor);
+                services.Replace(ServiceDescriptor.Describe(
+                  typeof(IPersistentStateFactory),
+                  s => new ActorStateFactory((IPersistentStateFactory)CreateInstance(s, descriptor)),
+                  descriptor.Lifetime
+                ));
             }
 
-            return state;
+            return services;
         }
 
-        class ActivationContext(object actor) : IRehydrationContext
+        static object CreateInstance(IServiceProvider services, ServiceDescriptor descriptor)
         {
-            public IEnumerable<string> Keys => throw new NotImplementedException();
-            public bool TryGetBytes(string key, out ReadOnlySequence<byte> value) => throw new NotImplementedException();
-            public bool TryGetValue<T>(string key, out T? value)
-            {
-                if (actor is T typed)
-                {
-                    value = typed;
-                    return true;
-                }
+            if (descriptor.ImplementationInstance is not null)
+                return descriptor.ImplementationInstance;
 
-                value = default;
-                return false;
-            }
+            if (descriptor.ImplementationFactory is not null)
+                return descriptor.ImplementationFactory(services);
+
+            return ActivatorUtilities.GetServiceOrCreateInstance(services, descriptor.ImplementationType!);
         }
     }
 }
-
---
+++ src/CloudActors.Streamstone/StreamstoneSiloBuilderExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using Devlooped.CloudActors;
+using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using Orleans.Providers;
+using Orleans.Runtime;
+using Orleans.Storage;
+
+namespace Orleans.Hosting;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class StreamstoneSiloBuilderExtensions
+{
+    public static ISiloBuilder AddStreamstoneActorStorage(this ISiloBuilder builder)
+        => builder.AddStreamstoneActorStorage(ProviderConstants.DEFAULT_STORAGE_PROVIDER_NAME);
+
+    public static ISiloBuilder AddStreamstoneActorStorage(this ISiloBuilder builder, string name)
+        => builder.ConfigureServices(services => services.AddStreamstoneActorStorage(name, null));
+
+    public static ISiloBuilder AddStreamstoneActorStorage(this ISiloBuilder builder, string name, Action<StreamstoneOptions> configure)
+        => builder.ConfigureServices(services => services.AddStreamstoneActorStorage(name, configure));
+
+    public static ISiloBuilder AddStreamstoneActorStorage(this ISiloBuilder builder, Action<StreamstoneOptions> configure)
+        => builder.ConfigureServices(services => services.AddStreamstoneActorStorage(ProviderConstants.DEFAULT_STORAGE_PROVIDER_NAME, configure));
+
+    internal static IServiceCollection AddStreamstoneActorStorage(
+        this IServiceCollection services,
+        string name,
+        Action<StreamstoneOptions>? configure = null)
+    {
+        if (configure is not null)
+            services.AddOptions<StreamstoneOptions>(name).Configure(configure);
+
+        if (string.Equals(name, ProviderConstants.DEFAULT_STORAGE_PROVIDER_NAME, StringComparison.Ordinal))
+        {
+            services.TryAddSingleton(sp => sp.GetServiceByName<IGrainStorage>(ProviderConstants.DEFAULT_STORAGE_PROVIDER_NAME));
+        }
+
+        return services.AddSingletonNamedService<IGrainStorage>(name, (sp, name) =>
+        {
+            var snapshot = sp.GetRequiredService<IOptionsMonitor<StreamstoneOptions>>();
+            return new StreamstoneStorage(sp.GetRequiredService<CloudStorageAccount>(), snapshot.Get(name));
+        });
+    }
+}
