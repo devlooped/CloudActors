007421fb958e8e0e0495bddf580994d28086c845
Daniel Cazzulino <daniel@cazzulino.com>
2024-07-29 18:22:37 -0300

    Improve support for validating indirectly used types
    Detect lacking partial type in indirect types (i.e. parameters to actor messages). We cannot provide code fixers for these because they have to be reported for an entire compilation (since we need to scan types and usages), which makes the experience suboptimal. But at least we don't fail at run-time.

--- src/CloudActors.CodeAnaysis/OrleansGenerator.cs
+++ src/CloudActors.CodeAnaysis/OrleansGenerator.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Xml.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
