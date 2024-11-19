c840fd5b714fce3d272936fb2e1ce9800dfff936
Daniel Cazzulino <daniel@cazzulino.com>
2024-11-19 15:54:01 -0300

    Minor docs and cleanup

--- src/CloudActors.Server/OrleansActorBus.cs
+++ src/CloudActors.Server/OrleansActorBus.cs
@@ -4,14 +4,20 @@ using Orleans.Runtime;
 
 namespace Devlooped.CloudActors;
 
+/// <summary>
+/// Implements the <see cref="IActorBus"/> interface over an <see cref="IGrainFactory"/>.
+/// </summary>
 public class OrleansActorBus(IGrainFactory factory) : IActorBus
 {
+    /// <inheritdoc/>
     public Task<TResult> ExecuteAsync<TResult>(string id, IActorCommand<TResult> command)
         => GetActor(id).ExecuteAsync(command);
 
+    /// <inheritdoc/>
     public Task ExecuteAsync(string id, IActorCommand command)
         => GetActor(id).ExecuteAsync(command);
 
+    /// <inheritdoc/>
     public Task<TResult> QueryAsync<TResult>(string id, IActorQuery<TResult> query)
         => GetActor(id).QueryAsync(query);
 
