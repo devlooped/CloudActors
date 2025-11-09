1e56c0f4109f6cc0e558eb2155242662dc1b576c
Daniel Cazzulino <daniel@cazzulino.com>
2025-11-09 20:24:27 -0300

    Allow relaxing snapshot version compatibility
    Rather than hardcoding a major+minor compatiiblity check between persisted state and current state assembly version, allow relaxing to major version only too.
    
    It's typical in SemVer to use strictly major-only bumps when introducing significant state/API breakage, so if the user uses that, we can relax the requirement on the snapshot reading. This can significantly improve performance when event steam is long and the app is frequently updated but with minor version bumps and no state breaking changes.

--- src/CloudActors.Streamstone/StreamstoneOptions.cs
+++ src/CloudActors.Streamstone/StreamstoneOptions.cs
@@ -3,6 +3,15 @@ using System.Text.Json.Serialization;
 
 namespace Devlooped.CloudActors;
 
+/// <summary>Specifies the level of version compatibility to use with snpashots.</summary>
+public enum SnapshotVersionCompatibility
+{
+    /// <summary>Consider serialized state snapshot with the same major version as compatible.</summary>
+    Major,
+    /// <summary>Consider serialized state snapshot with the same major and minor version as compatible.</summary>
+    Minor,
+}
+
 /// <summary>Provides options for configuring <see cref="StreamstoneStorage"/> behavior.</summary>
 public class StreamstoneOptions
 {
@@ -20,6 +29,19 @@ public class StreamstoneOptions
 
     /// <summary>When true, will automatically create a snapshot of the state for easy retrieval.</summary>
     public bool AutoSnapshot { get; set; } = true;
+
+    /// <summary>The snapshot version compatibility to use when deserializing state.</summary>
+    /// <remarks>
+    /// When <see cref="AutoSnapshot"/> is true, snapshots will be created automatically on save, 
+    /// and when loading state, the most recent snapshot will be used as a starting point to avoid 
+    /// reading and replaying all events from the stream. This can greatly improve performance.
+    /// <para>
+    /// This property controls how version compatibility is determined when deserializing snapshots.
+    /// By default, snapshots are considered compatible if the serialized state major and minor 
+    /// version match with the current actor state assembly version.
+    /// </para>
+    /// </remarks>
+    public SnapshotVersionCompatibility SnapshotCompatibility { get; set; } = SnapshotVersionCompatibility.Minor;
 
     /// <summary>The settings to use when serializing and deserializing events, and snapshot if <see cref="AutoSnapshot"/> is true.</summary>
     public JsonSerializerOptions JsonOptions { get; set; } = options;
