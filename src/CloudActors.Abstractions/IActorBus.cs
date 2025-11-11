439be8322769cf40b37f267f126f86cc18c97bea
Daniel Cazzulino <daniel@cazzulino.com>
2025-11-11 20:00:06 -0300

    Instrument the actor bus with standard-ish telemetry
    Leverage the standard OTEL semantic conventions for messaging from https://opentelemetry.io/docs/specs/semconv/messaging/messaging-spans/#operation-types and https://opentelemetry.io/docs/specs/semconv/messaging/messaging-spans/#span-name.

--- src/CloudActors.Abstractions/IActorBus.cs
+++ src/CloudActors.Abstractions/IActorBus.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace Devlooped.CloudActors;
 
@@ -12,7 +14,7 @@ public interface IActorBus
     /// </summary>
     /// <param name="id">The actor identifier, such as <c>account/1</c>.</param>
     /// <param name="command">The command to execute.</param>
-    Task ExecuteAsync(string id, IActorCommand command);
+    Task ExecuteAsync(string id, IActorCommand command) => ExecuteAsync(id, command, null, null, null);
 
     /// <summary>
     /// Invokes a state-changing operation on an actor which returns a value.
@@ -20,7 +22,7 @@ public interface IActorBus
     /// <typeparam name="TResult">Type of returned value, typically inferred from the <paramref name="command"/>.</typeparam>
     /// <param name="id">The actor identifier, such as <c>account/1</c>.</param>
     /// <param name="command">The command to execute.</param>
-    Task<TResult> ExecuteAsync<TResult>(string id, IActorCommand<TResult> command);
+    Task<TResult> ExecuteAsync<TResult>(string id, IActorCommand<TResult> command) => ExecuteAsync(id, command, null, null, null);
 
     /// <summary>
     /// Invokes a read-only query on an actor.
@@ -28,5 +30,46 @@ public interface IActorBus
     /// <typeparam name="TResult">Type of returned value, typically inferred from the <paramref name="query"/>.</typeparam>
     /// <param name="id">The actor identifier, such as <c>account/1</c>.</param>
     /// <param name="query">The query to execute.</param>
-    Task<TResult> QueryAsync<TResult>(string id, IActorQuery<TResult> query);
+    Task<TResult> QueryAsync<TResult>(string id, IActorQuery<TResult> query) => QueryAsync(id, query, null, null, null);
+
+    // Hidden, prioritized overload for automatic telemetry capture.
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [OverloadResolutionPriority(1)]
+    /// <summary>
+    /// Invokes a state-changing operation on an actor.
+    /// </summary>
+    /// <param name="id">The actor identifier, such as <c>account/1</c>.</param>
+    /// <param name="command">The command to execute.</param>
+    /// <param name="callerName">Optional calling member name, provided by default by the compiler.</param>
+    /// <param name="callerFile">Optional calling file name, provided by default by the compiler.</param>
+    /// <param name="callerLine">Optional calling line number, provided by default by the compiler.</param>
+    Task ExecuteAsync(string id, IActorCommand command, [CallerMemberName] string? callerName = default, [CallerFilePath] string? callerFile = default, [CallerLineNumber] int? callerLine = default);
+
+    // Hidden, prioritized overload for automatic telemetry capture.
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [OverloadResolutionPriority(1)]
+    /// <summary>
+    /// Invokes a state-changing operation on an actor which returns a value.
+    /// </summary>
+    /// <typeparam name="TResult">Type of returned value, typically inferred from the <paramref name="command"/>.</typeparam>
+    /// <param name="id">The actor identifier, such as <c>account/1</c>.</param>
+    /// <param name="command">The command to execute.</param>
+    /// <param name="callerName">Optional calling member name, provided by default by the compiler.</param>
+    /// <param name="callerFile">Optional calling file name, provided by default by the compiler.</param>
+    /// <param name="callerLine">Optional calling line number, provided by default by the compiler.</param>
+    Task<TResult> ExecuteAsync<TResult>(string id, IActorCommand<TResult> command, [CallerMemberName] string? callerName = default, [CallerFilePath] string? callerFile = default, [CallerLineNumber] int? callerLine = default);
+
+    // Hidden, prioritized overload for automatic telemetry capture.
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [OverloadResolutionPriority(1)]
+    /// <summary>
+    /// Invokes a read-only query on an actor.
+    /// </summary>
+    /// <typeparam name="TResult">Type of returned value, typically inferred from the <paramref name="query"/>.</typeparam>
+    /// <param name="id">The actor identifier, such as <c>account/1</c>.</param>
+    /// <param name="query">The query to execute.</param>
+    /// <param name="callerName">Optional calling member name, provided by default by the compiler.</param>
+    /// <param name="callerFile">Optional calling file name, provided by default by the compiler.</param>
+    /// <param name="callerLine">Optional calling line number, provided by default by the compiler.</param>
+    Task<TResult> QueryAsync<TResult>(string id, IActorQuery<TResult> query, [CallerMemberName] string? callerName = default, [CallerFilePath] string? callerFile = default, [CallerLineNumber] int? callerLine = default);
 }
---
+++ src/CloudActors/Telemetry.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+namespace Devlooped.CloudActors;
+
+static class Telemetry
+{
+    static readonly ActivitySource tracer = new(nameof(CloudActors), ThisAssembly.Info.Version);
+    static Meter Meter { get; } = new Meter(nameof(CloudActors), ThisAssembly.Info.Version);
+
+    static readonly Counter<long> commands;
+    static readonly Counter<long> queries;
+
+    /// <summary>Duration of commands executed by the CloudActors bus.</summary>
+    public static Histogram<long> Processing { get; } =
+        Meter.CreateHistogram<long>("Processing", unit: "ms", description: "Duration of commands executed by the CloudActors bus.");
+
+    /// <summary>Duration of queries send to the CloudActors bus.</summary>
+    public static Histogram<long> Sending { get; } =
+        Meter.CreateHistogram<long>("Sending", unit: "ms", description: "Duration of queries sent to the CloudActors bus.");
+
+    static Telemetry()
+    {
+        commands = Meter.CreateCounter<long>("Commands", description: "Commands executed by the CloudActors bus.");
+        queries = Meter.CreateCounter<long>("Queries", description: "Queries run by the CloudActors bus.");
+    }
+
+    // process: A message that was previously received from a destination is processed by a message consumer/server.
+    public const string Process = "process";
+
+    // send: A one-way message (query) is sent to a destination.
+    public const string Send = "send";
+
+    public static Activity? StartCommandActivity(object command, string? callerName, string? callerFile, int? callerLine)
+        => StartActivity(Process, command, callerName, callerFile, callerLine);
+
+    public static Activity? StartQueryActivity(object query, string? callerName, string? callerFile, int? callerLine)
+        => StartActivity(Send, query, callerName, callerFile, callerLine);
+
+    public static Activity? StartActivity(string operation, object command, string? callerName, string? callerFile, int? callerLine)
+    {
+        ThrowIfNull(command);
+        var type = command.GetType();
+
+        if (operation == Send)
+            queries.Add(1, new KeyValuePair<string, object?>("Name", type.FullName));
+        else if (operation == Process)
+            commands.Add(1, new KeyValuePair<string, object?>("Name", type.FullName));
+
+        // Span name convention should be: {messaging.operation.name} {destination} (see https://opentelemetry.io/docs/specs/semconv/messaging/messaging-spans/#span-name)
+        // Requirement is that the destination has low cardinality.
+        var activity = tracer.CreateActivity($"process {type.FullName}", ActivityKind.Producer)
+            ?.SetTag("code.function", callerName)
+            ?.SetTag("code.filepath", callerFile)
+            ?.SetTag("code.lineno", callerLine)
+            ?.SetTag("messaging.system", "cloudactors")
+            ?.SetTag("messaging.destination.name", type.FullName)
+            ?.SetTag("messaging.destination.kind", "topic")
+            ?.SetTag("messaging.operation", "process")
+            ?.SetTag("messaging.protocol.name", type.Assembly.GetName().Name)
+            ?.SetTag("messaging.protocol.version", type.Assembly.GetName().Version?.ToString() ?? "unknown");
+
+        if (command != null &&
+            // Additional optimization so we don't incur allocation of activity custom props storage 
+            // unless someone is actually requesting the data. See https://github.com/open-telemetry/opentelemetry-dotnet/issues/1397 
+            activity?.IsAllDataRequested == true)
+            activity.SetCustomProperty(operation == Send ? "query" : "command", command);
+
+        activity?.Start();
+
+        return activity;
+    }
+
+    public static void SetException(this Activity? activity, Exception e)
+    {
+        if (activity != null)
+        {
+            activity.SetStatus(ActivityStatusCode.Error, e.Message);
+
+            activity.SetTag("otel.status_code", "ERROR");
+            activity.SetTag("otel.status_description", e.Message);
+
+            // See https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/exceptions.md
+            activity.AddEvent(new ActivityEvent("exception", tags: new()
+            {
+                { "exception.message", e.Message },
+                { "exception.type", e.GetType().FullName },
+                { "exception.stacktrace", e.ToString() },
+            }));
+        }
+    }
+}
